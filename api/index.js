b96b7a4707ef60947b846fcef283b5af0d3986d2
KATHIRVEL133 <velkathir259@gmail.com>
2024-09-07 14:22:14 +0530

    created api route for signUp functionality and tested it

--- api/index.js
+++ api/index.js
@@ -1,15 +1,28 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import signUpRoute from './routes/auth.route.js'
 const app = express();
 dotenv.config();
 const PORT = 3000 || process.env.PORT;
+app.use(express.json());
 app.listen(PORT,()=>
 {
     console.log(`Server is running successfully on the port ${PORT}`)
 })
+app.use('/api/auth',signUpRoute)
 mongoose.connect(process.env.MONGO_DB).then(()=>{
     console.log('Connected to MongoDB');
 }).catch((err)=>{
     console.log(error);
+});
+app.use((err,req,res,next)=>
+{
+    const statusCode = err.statusCode||500;
+    const message = err.message||'Internal Server Error';
+    return res.json({
+        success:false,
+        statusCode,
+        message
+    })
 })
--- client/to-do/src/pages/SignUp.jsx
+++ client/to-do/src/pages/SignUp.jsx
@@ -2,17 +2,43 @@
 import { useState } from "react";
 export default function SignUp() {
   const [formData,setFormData] = useState({});
+  const [error,setError] = useState(null);
   const handleChange = (e)=>
   {
     setFormData({...formData,[e.target.id]:e.target.value})
   }
-  
+  const handleSubmit = async (e)=>
+  {
+    e.preventDefault();
+    try
+    {
+      setError(null);
+      const res = await fetch('/api/auth/sign-up',{
+        method:'POST',
+        headers:{
+          'Content-Type':'application/json',
+        },
+        body:JSON.stringify(formData)
+      });
+      const data = await res.json();
+      if(data.success===false)
+      {
+        setError(data.message);
+        return;
+      }
+      console.log(data);
+    }
+    catch(error)
+    {
+      setError(error);
+    }
+  }
   return (
     <div className="flex flex-col justify-between items-center mt-52 gap-5"> 
       <h1 className="font-semibold text-2xl">
         Sign Up
       </h1>
-      <form className="flex flex-col gap-3 w-[300px] sm:w-[350px]">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-3 w-[300px] sm:w-[350px]">
         <input type="text" placeholder="Username" id="username"  onChange={handleChange} className="p-3 border rounded-md"/>
         <input type="email" placeholder="Enter your email" id="email"  onChange={handleChange} className="p-3 border rounded-md"/>
         <input type="password" placeholder="Password" id="password"  onChange={handleChange} className="p-3 border rounded-md"/>
@@ -20,6 +46,11 @@ export default function SignUp() {
           Sign Up
         </button>
       </form>
+      {
+        error&&<p className="text-sm text-red-600">
+          {error}
+        </p>
+      }
     </div>
   )
 }
